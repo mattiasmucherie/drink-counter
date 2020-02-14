930c4e299711d2184091d90571d6f89de82305d7
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-02-14 01:19:52 +0100

    Change .env variables

--- src/components/Firebase/firebase.ts
+++ src/components/Firebase/firebase.ts
@@ -1,14 +1,15 @@
 import app from "firebase/app";
 import "firebase/auth";
+
 const config = {
-  apiKey: process.env.DRINK_COUNTER_API_KEY,
-  authDomain: process.env.DRINK_COUNTER_AUTH_DOMAIN,
-  databaseURL: process.env.DRINK_COUNTER_DATABASE_URL,
-  projectId: process.env.DRINK_COUNTER_PROJECT_ID,
-  storageBucket: process.env.DRINK_COUNTER_STORAGE_BUCKET,
-  messagingSenderId: process.env.DRINK_COUNTER_MESSAGING_SENDER_ID,
-  appId: process.env.DRINK_COUNTER_APP_ID,
-  measurementID: process.env.DRINK_COUNTER_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementID: process.env.REACT_APP_MEASUREMENT_ID
 };
 class Firebase {
   auth: app.auth.Auth;

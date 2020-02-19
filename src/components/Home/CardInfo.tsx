caacced585adbe33bb16ee83556a361b2f11286c
Mattias Mucherie <mattias.mucherie@scania.com>
2020-02-19 16:02:04 +0100

    Added a small test

--- src/components/Home/CardInfo.tsx
+++ src/components/Home/CardInfo.tsx
@@ -6,11 +6,11 @@ interface TeamProps {
 }
 const CardInfo: React.FC<TeamProps> = props => {
   return (
-    <div className="card">
+    <div data-testid="card-info-team-card" className="card">
       <div className="title">{props.team.name}</div>
       <div className="total">{props.team.total}</div>
       <div className="link-to-team">
-        <Link to={`/team/${props.team.id}`}>
+        <Link data-testid="card-info-link" to={`/team/${props.team.id}`}>
           <i className="fas fa-arrow-right"></i>
         </Link>
       </div>
---
+++ src/components/Home/__test__/CardInfo.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CardInfo from "../CardInfo";
+import { render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const mountComponent = (team: any) => {
+  return render(
+    <Router history={createBrowserHistory()}>
+      <CardInfo team={team} />
+    </Router>
+  );
+};
+describe("Mounting Component", () => {
+  const team = {
+    name: "Test",
+    total: 0,
+    owner: "SomeRandomId",
+    id: "Team-ID"
+  };
+  const { getByTestId } = mountComponent(team);
+  it("renders without crashing", () => {
+    const card = getByTestId("card-info-team-card");
+    const link = getByTestId("card-info-link");
+    expect(link.closest("a")).toHaveAttribute("href", "/team/Team-ID");
+    expect(card).toBeInTheDocument();
+  });
+});
